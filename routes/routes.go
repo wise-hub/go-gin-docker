d4e1d30b1c5a69760f35e8e2e7ed81142bb49b94
VI <vladimir.ikonomov@gmail.com>
2023-03-21 23:47:46 +0200

    lean

--- routes/routes.go
+++ routes/routes.go
@@ -15,18 +15,16 @@ func Routes(d *config.Dependencies) *gin.Engine {
 	r.POST("/api/login", handler.UserLoginHandler(d))
 	r.Use(middleware.LogMiddleware(d))
 
-	api := r.Group("/api")
-	api.Use(middleware.AuthMiddleware(d))
-	api.Use(middleware.RoleMiddleware(d))
-
 	{
+		api := r.Group("/api")
+		api.Use(middleware.AuthMiddleware(d))
+		api.Use(middleware.RoleMiddleware(d))
+
 		api.GET("/customer/:id", handler.CustomerHandler(d))
 		api.GET("/accounts/:id", handler.AccountsHandler(d))
 		api.GET("/customer-accounts/:id", handler.CustAccHandler(d))
-		// cust feedback
-		api.POST("/customer-feedback", handler.CustFeedbackAddHandler(d))
 		api.GET("/customer-feedback/:id", handler.CustFeedbackReadHandler(d))
-		// others below
+		api.POST("/customer-feedback", handler.CustFeedbackAddHandler(d))
 	}
 
 	return r
